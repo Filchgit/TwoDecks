f987b3618c83e12368c7e4f632ef186ab1a450f4
John MacAulay <merjohn@bigpond.net.au>
2021-07-05 14:26:54 +1000

    still not doiing local resiources

---
+++ Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TwoDecksWPF
+{
+    using System.Collections.ObjectModel;
+    class Deck : ObservableCollection<Card>
+    {
+        // filling this in soon
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TwoDecks
+namespace TwoDecksWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,5 +24,7 @@ namespace TwoDecks
         {
             InitializeComponent();
         }
+
+       
     }
 }
